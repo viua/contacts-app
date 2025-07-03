16f1a24e0c56ef32f8378aee4b4b6568b87ba2fd
viua <vlad.ivashevich@gmail.com>
2025-07-03 18:16:26 +0200

    EditContactComponent: finalizing

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -37,24 +37,15 @@ export class EditContactComponent implements OnInit {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
 
-    //this.contactsService.getContact(contactId).subscribe({});
     this.contactsService.getContact(contactId).subscribe((contact) => {
       if (!contact) {
         return;
       }
       this.contactForm.setValue(contact);
-      /* this.contactForm.controls.id.setValue(contact.id);
-      this.contactForm.controls.firstName.setValue(contact.firstName);
-      this.contactForm.controls.lastName.setValue(contact.lastName);
-      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
-      this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking)
-      this.contactForm.controls.phone.controls.phoneNumber.setValue(contact.phone.phoneNumber)
-      this.contactForm.controls.phone.controls.phoneType.setValue(contact.phone.phoneType)
-      this.contactForm.controls.address.controls.streetAddress.setValue(contact.address.streetAddress)
-      this.contactForm.controls.address.controls.city.setValue(contact.address.city)
-      this.contactForm.controls.address.controls.state.setValue(contact.address.state)
-      this.contactForm.controls.address.controls.postalCode.setValue(contact.address.postalCode)
-      this.contactForm.controls.address.controls.addressType.setValue(contact.address.addressType) */
+
+      /// *** partial filling form ***
+      /// const names = {firstName: contact.firstName, lastName: contact.lastName};
+      /// this.contactForm.patchValue(names);
     });
   }
 
