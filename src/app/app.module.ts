372cf1134c660264bc1349d0cf50a6d03d54616b
viua <vlad.ivashevich@gmail.com>
2025-07-03 17:32:41 +0200

    EditContactComponent: first updatet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
 import { InMemoryContactsApi } from './contacts/in-memory-contacts.service';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,12 +16,13 @@ import { InMemoryContactsApi } from './contacts/in-memory-contacts.service';
     ContactListComponent,
     EditContactComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryContactsApi, { delay: 200 })
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryContactsApi, {delay: 200}),
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,19 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormControl, FormGroup} from "@angular/forms";
+import {ContactsService} from "../contacts/contacts.service";
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private contactsService: ContactsService, private router: Router) {
+  }
+
+  contactForm = new FormGroup({
+    id: new FormControl(),
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    dateOfBirth: new FormControl(),
+    favoritesRanking: new FormControl(),
+    phone: new FormGroup({
+      phoneNumber: new FormControl(),
+      phoneType: new FormControl(),
+    }),
+    address: new FormGroup({
+      streetAddress: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      postalCode: new FormControl(),
+      addressType: new FormControl()
+    })
+  });
+
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    //this.contactsService.getContact(contactId).subscribe({});
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      if (!contact) {
+        return;
+      }
+      this.contactForm.controls.id.setValue(contact.id);
+      this.contactForm.controls.firstName.setValue(contact.firstName);
+      this.contactForm.controls.lastName.setValue(contact.lastName);
+      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
+      this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking)
+      this.contactForm.controls.phone.controls.phoneNumber.setValue(contact.phone.phoneNumber)
+      this.contactForm.controls.phone.controls.phoneType.setValue(contact.phone.phoneType)
+      this.contactForm.controls.address.controls.streetAddress.setValue(contact.address.streetAddress)
+      this.contactForm.controls.address.controls.city.setValue(contact.address.city)
+      this.contactForm.controls.address.controls.state.setValue(contact.address.state)
+      this.contactForm.controls.address.controls.postalCode.setValue(contact.address.postalCode)
+      this.contactForm.controls.address.controls.addressType.setValue(contact.address.addressType)
+    });
   }
 
   saveContact() {
-
+    console.log("saving=>", this.contactForm.value);
+    this.contactsService.saveContact(this.contactForm.getRawValue()).subscribe({
+    /// this.contactsService.saveContact(this.contactForm.value).subscribe({
+      next: () => this.router.navigate(['/contacts'])
+    });
   }
 }
